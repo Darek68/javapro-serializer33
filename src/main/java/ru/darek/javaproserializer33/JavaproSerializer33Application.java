e793ef72219dafc1f54a85b5f9f5b2b45626b428
Darek <darek68@list.ru>
2024-07-14 17:58:09 +0300

    MasterCommitFirst

---
+++ src/main/java/ru/darek/javaproserializer33/JavaproSerializer33Application.java
@@ -0,0 +1,13 @@
+package ru.darek.javaproserializer33;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaproSerializer33Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaproSerializer33Application.class, args);
+    }
+
+}
